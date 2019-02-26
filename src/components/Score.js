abaa933399ff835049f0c819b12a513ce51c8744
Mares Popa <popa.mares@gmail.com>
2019-02-26 14:17:20 +0200

    Update Changes

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const Score = ({readCount, total}) => {
-  const percentRead = readCount/total * 100;
-  const percentStyle = {width: percentRead + '%'};
-  const uncompletedMessage = (percentRead !== 100) && (<span>{readCount} out of {total}</span>);
-  const completedMessage = (percentRead === 100) && (<span>Completed!</span>);
+const Score = ({ readCount, total }) => {
+  const percentRead = (readCount / total) * 100;
+  const percentStyle = { width: percentRead + "%" };
+  const uncompletedMessage = percentRead !== 100 && (
+    <span>
+      read {readCount} of {total}
+    </span>
+  );
+  const completedMessage = percentRead === 100 && <span>Completed!</span>;
 
   return (
-  <section className="read-status">
-    <div className="progress-bar">
-        <span className="progress" style={percentStyle}></span>
-    </div>
-    {uncompletedMessage}
-    {completedMessage}
-  </section>
-)};
+    <section className="read-status">
+      <div className="progress-bar">
+        <span className="progress" style={percentStyle} />
+      </div>
+      {uncompletedMessage}
+      {completedMessage}
+    </section>
+  );
+};
 
 export default Score;
